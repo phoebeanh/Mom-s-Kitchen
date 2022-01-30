a37532b47f36c0454f87e91874987822d7fa11b2
snehap9 <snehap.sne@gmail.com>
2022-01-29 17:53:15 -0800

    Add Firebase integration
    Add Firebase integration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,20 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+
+    apiKey: "AIzaSyAA8er--_1Jue7s4LaPkHIfOAKvQolL10k",
+  
+    authDomain: "cs633-moms-kitchen.firebaseapp.com",
+  
+    projectId: "cs633-moms-kitchen",
+  
+    storageBucket: "cs633-moms-kitchen.appspot.com",
+  
+    messagingSenderId: "476263298804",
+  
+    appId: "1:476263298804:web:08937f58dc90ba924d50a1",
+  
+    measurementId: "G-4TDPE4R24W"
+  
+  }
 };
