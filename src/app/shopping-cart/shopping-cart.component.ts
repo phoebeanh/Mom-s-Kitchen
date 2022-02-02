44bc77f3d78a4cec75976955dd4fa59f96c10b91
luximeng <lucymeng2009@gmail.com>
2022-02-01 21:06:52 -0800

    shopping-cart

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -13,3 +13,34 @@ export class ShoppingCartComponent implements OnInit {
   }
 
 }
+
+import { FormBuilder } from '@angular/forms';
+
+import { ShoppingCartService } from '../services/shopping-cart.service';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './shopping-cart.component.html',
+  styleUrls: ['./shopping-cart.component.css']
+})
+export class CartComponent {
+
+  items = this.cartService.getItems();
+
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  });
+
+  constructor(
+    private cartService: ShoppingCartService,
+    private formBuilder: FormBuilder,
+  ) {}
+
+  onSubmit(): void {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
+}
